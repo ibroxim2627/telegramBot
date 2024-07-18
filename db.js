94bd3173e41bc08fb9f61bda11c0a8e47f504ebc
IBROXIM TO'LAGANOV <ibroximtolaganov10@gmail.com>
2024-07-18 16:17:58 +0500

    make it better

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
-    host: 'localhost',
+    host: '198.199.72.220',
     database: 'bot',
-    password: '2627',
+    password: 'Pn6ZNRZMDQRZadCN',
     port: 5432,
 });
 
